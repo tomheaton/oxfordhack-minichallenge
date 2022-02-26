0ab5fd89927f17c60c26a4f4decfb70aff510fda
= <thomas.m.heaton@gmail.com>
2022-02-26 13:23:35 +0000

    add org card

---
+++ pages/components/card.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Org } from "../types/types";
+import styles from "../../styles/Card.module.css";
+
+const Card: React.FC<{org: Org}> = ({ org }) => {
+
+    return (
+        <div className={styles.card}>
+            {/*{JSON.stringify(org, null, 4)}*/}
+            <p>name: {org.name}</p>
+            <p>address: {org.address}</p>
+            <p>organization_type: {org.organization_type}</p>
+            <p>phone_number: {org.phone_number}</p>
+            <p>Shared Comments: {org["Shared Comments"]}</p>
+            <p>Shared Tickets: {org["Shared Tickets"]}</p>
+            <p>ssn: {org.ssn}</p>
+        </div>
+    );
+}
+
+export default Card;
---
+++ pages/types/types.ts
@@ -0,0 +1,13 @@
+type Org = {
+    name: string
+    address: string
+    organization_type: string
+    outlet_type: string
+    owner: string
+    phone_number: string
+    "Shared Comments": string
+    "Shared Tickets": string
+    ssn: string
+}
+
+export type { Org };
